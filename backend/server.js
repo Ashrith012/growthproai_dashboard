125708377a2f4868b6743f353b05e3f5711b4024
Ashrith <132201845+Ashrith012@users.noreply.github.com>
2025-07-05 08:15:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use("/api",businessRoutes);
 
 
 
-const port = 5000;
+const PORT = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server is Running on port ${port}`)
 });
